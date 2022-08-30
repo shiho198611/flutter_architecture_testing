2e5b530b627880ec92792cdef5af7c80ff54644e
shiho198611 <shiho198611@gmail.com>
2022-08-30 15:39:31 +0800

    style: add dragons list

--- lib/widget/dragon_wiget.dart
+++ lib/widget/dragon_wiget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_architecture_testing/bloc/dragon_bloc.dart';
 import 'package:flutter_architecture_testing/data/dragon_repository.dart';
 import 'package:flutter_architecture_testing/data/space_x_remote_service.dart';
@@ -7,13 +6,13 @@ import 'package:flutter_architecture_testing/state/dragon_list_state.dart';
 import 'package:flutter_architecture_testing/state/query_action_status.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DragonWidget extends StatelessWidget {
   const DragonWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     return Provider(
       create: (_) => DragonRepository(context.read<SpaceXRemoteService>()),
       child: Scaffold(
@@ -26,20 +25,18 @@ class DragonWidget extends StatelessWidget {
   }
 }
 
-
 class DragonListBlocWidget extends StatelessWidget {
   const DragonListBlocWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-        create: (_)  => DragonBloc(context.read<DragonRepository>())..add(DragonListQuery()),
-        child: const DragonList(),
+      create: (_) =>
+          DragonBloc(context.read<DragonRepository>())..add(DragonListQuery()),
+      child: const DragonList(),
     );
   }
-
 }
-
 
 class DragonListRouteState extends State<DragonList> with RouteAware {
   @override
@@ -54,13 +51,53 @@ class DragonListRouteState extends State<DragonList> with RouteAware {
           } else {
             return ListView.builder(
               itemBuilder: (context, int index) {
-                  final dragon = status.remoteData[index];
+                final dragon = status.remoteData[index];
+                final imageUrl = dragon.flickrImages?.first;
+                final name = (dragon.name != null) ? dragon.name! : "";
+                final wikiUrl =
+                    (dragon.wikipedia != null) ? dragon.wikipedia! : "";
 
-                  return ListTile(
-                    title: Text(dragon.description!),
-                  );
+                return Column(children: [
+                  Row(
+                    children: [
+                      (imageUrl == null)
+                          ? const Text("No Image")
+                          : Padding(
+                              padding: const EdgeInsets.all(20),
+                              child: Image.network(imageUrl,
+                                  width: 150, height: 150)),
+                      Align(
+                          alignment: Alignment.topLeft,
+                          child: Container(
+                            height: 150,
+                            padding: const EdgeInsets.only(top: 20),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(name,
+                                    style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                                Padding(
+                                    padding: const EdgeInsets.only(top: 20),
+                                    child: GestureDetector(
+                                      child: const Text("Go to wiki",
+                                          style: TextStyle(
+                                              color: Colors.blue,
+                                              decoration:
+                                                  TextDecoration.underline)),
+                                      onTap: () async {
+                                        await launchUrl(Uri.parse(wikiUrl));
+                                      },
+                                    ))
+                              ],
+                            ),
+                          )),
+                    ],
+                  ),
+                  const Divider()
+                ]);
               },
-              itemCount: status.remoteData.length,);
+              itemCount: status.remoteData.length,
+            );
           }
         } else {
           return const Center(
@@ -70,9 +107,7 @@ class DragonListRouteState extends State<DragonList> with RouteAware {
       },
     );
   }
-
 }
-
 
 class DragonList extends StatefulWidget {
   const DragonList({Key? key}) : super(key: key);
@@ -81,6 +116,4 @@ class DragonList extends StatefulWidget {
   State<StatefulWidget> createState() {
     return DragonListRouteState();
   }
-
-
 }
